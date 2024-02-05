637293fbf0a76f6fb832d51d96b37742bdeab346
Josiah Putman <joshikatsu@gmail.com>
2024-02-04 17:16:36 -0800

    Fix waypoint zindex

--- src/zindex.rs
+++ src/zindex.rs
@@ -3,7 +3,6 @@
 pub const BACKGROUND: f32 = -10.0;
 pub const SHADER_BACKGROUND: f32 = -9.0;
 pub const OBSTACLES: f32 = -7.0;
-pub const WAYPOINT: f32 = -5.0;
 pub const FOOD_BACKGROUND: f32 = -2.1;
 pub const FOOD: f32 = -2.0;
 pub const ZOOID_HEAD_BACKGROUND: f32 = -0.1;
@@ -14,6 +13,7 @@ pub const ZOOIDS_MAX: f32 = 10.0;
 pub const NAVIGATION_LAYER: f32 = 11.0;
 pub const HIGHLIGHT: f32 = 15.0;
 pub const FOG_OF_WAR: f32 = 20.0;
+pub const WAYPOINT: f32 = 22.0;
 pub const MINIMAP: f32 = 25.0;
 pub const SELECTOR: f32 = 30.0;
 pub const CURSOR: f32 = 35.0;
